ddeab28c3529a9514d4effb772d45e24a035986c
OmariSyn <sonicknight3000@gmail.com>
2026-09-24 21:41:22 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace MauiBasicAuth
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            // Get the username and password from the entry fields
+            string username = txtUserId.Text;
+            string password = txtPassword.Text;
+
+            // Create an instance of UserAuthentication
+            var userAuth = new DataAccess.UserAuthentication();
+
+            // Authenticate the user
+            bool isAuthenticated = userAuth.AuthenticateUser(username, password);
+
+            // Display the result
+            if (isAuthenticated)
+            {
+                DisplayAlert("Success", "User authenticated successfully!", "OK");
+            }
+            else
+            {
+                DisplayAlert("Error", "Invalid username or password.", "OK");
+            }
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiBasicAuth;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Register services here
+        // Example: builder.Services.AddSingleton<YourService>();
+
+        return builder.Build();
+    }
+}
